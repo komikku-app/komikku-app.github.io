23157e2a57fe860a935f040678900faa93b80ed3
Cuong-Tran <16017808+cuong-tran@users.noreply.github.com>
2025-12-01 12:48:45 +0700

    Fix logo

--- website/src/.vitepress/config/headConfig.ts
+++ website/src/.vitepress/config/headConfig.ts
@@ -2,8 +2,8 @@ import type { HeadConfig } from 'vitepress'
 
 const headConfig: HeadConfig[] = [
   ['meta', { name: 'darkreader-lock' }],
-  ['meta', { name: 'theme-color', content: '#0058A0' }],
-  ['meta', { name: 'msapplication-TileColor', content: '#0058A0' }],
+  ['meta', { name: 'theme-color', content: '#ff5555' }],
+  ['meta', { name: 'msapplication-TileColor', content: '#ff5555' }],
   ['meta', { name: 'viewport', content: 'width=device-width, initial-scale=1.0' }],
   ['meta', { name: 'referrer', content: 'no-referrer-when-downgrade' }],
   ['meta', { name: 'algolia-site-verification', content: '037010E1F9C05EF7' }],
@@ -11,7 +11,7 @@ const headConfig: HeadConfig[] = [
   ['link', { rel: 'icon', type: 'image/png', sizes: '32x32', href: '/favicon-32x32.png' }],
   ['link', { rel: 'icon', type: 'image/png', sizes: '16x16', href: '/favicon-16x16.png' }],
   ['link', { rel: 'manifest', href: '/site.webmanifest' }],
-  ['link', { rel: 'mask-icon', href: '/safari-pinned-tab.svg', color: '#0058A0' }],
+  ['link', { rel: 'mask-icon', href: '/safari-pinned-tab.svg', color: '#ff5555' }],
   ['link', { rel: 'apple-touch-icon', sizes: '180x180', href: '/apple-touch-icon.png' }],
   ['meta', { name: 'twitter:card', content: 'summary' }],
   ['meta', { property: 'og:site_name', content: 'Komikku' }],
